ae3166041e68ef6690f8d5a3b2da75d1bccf8d64
Martin Hinshelwood (nkdAgility) <martin@nkdagility.com>
2015-12-17 11:09:00 +0100

    Added demo app + updated UI

---
+++ TfvcBranchPolicy.CheckinPolicy.DemoUI/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using TfvcBranchPolicy.CheckinPolicy.Common;
+using TfvcBranchPolicy.CheckinPolicy.Editor;
+using TfvcBranchPolicy.Common;
+
+namespace TfvcBranchPolicy.CheckinPolicy.DemoUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<BranchPattern> branchPatterns = new List<BranchPattern>();
+            var wpfwindow = new BranchLockPolicyEditorWindow(branchPatterns);
+            ElementHost.EnableModelessKeyboardInterop(wpfwindow);
+            wpfwindow.ShowDialog();
+            branchPatterns = wpfwindow.ViewModel.GetBranchPatterns().ToList();
+            this.Close();
+        }
+    }
+}
